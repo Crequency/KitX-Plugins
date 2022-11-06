3b7891c3d19336d18c1b302cbe8b292ab138b0b6
Dynesshely <catrol@qq.com>
2022-11-07 04:54:30 +0800

    💾 Feat(TestPlugin.WPF.Core): 重构的 Function 结构

--- TestPlugin.WPF.Core/Controller.cs
+++ TestPlugin.WPF.Core/Controller.cs
@@ -28,43 +28,30 @@ namespace TestPlugin.WPF.Core
             mainwin.Show();
         }
 
-        public Function GetFunctions()
+        public List<Function> GetFunctions()
         {
-            return new Function()
+            return new List<Function>()
             {
-                FunctionsDisplayName = new Dictionary<string, Dictionary<string, string>>()
+                new Function()
                 {
+                    DisplayNames = new Dictionary<string, string>()
                     {
-                        "Default",
-                        new Dictionary<string, string>()
-                        {
-                            { "zh-cn", "嗨嗨嗨" }
-                        }
-                    }
-                },
-                FunctionParameters = new Dictionary<string, Function.ParameterList>()
-                {
+                        { "zh-cn", "你好, 世界!" },
+                        { "en-us", "Hello, World!" }
+                    },
+                    Parameters = new Dictionary<Dictionary<string, string>, string>()
                     {
-                        "Default",
-                        new Function.ParameterList()
                         {
-                            HasAppendParameters = false,
-                            AppendParameterType = "",
-                            ForeParameters = new List<Function.ParameterList.Parameter>()
+                            new Dictionary<string, string>()
                             {
-                                new Function.ParameterList.Parameter()
-                                {
-                                    DisplayName = new Dictionary<string, string>()
-                                    {
-                                        { "zh-cn", "哈哈哈" }
-                                    },
-                                    Name = "hhh",
-                                    Type = "void"
-                                }
+                                { "zh-cn", "参数1" },
+                                { "en-us", "Parameter1" }
                             },
-                            ReturnValueType = "void"
+                            "void"
                         }
-                    }
+                    },
+                    HasAppendParameters = false,
+                    ReturnValueType = "void"
                 }
             };
         }
