508dc3f2b30bafc109ecc1d64d898d1e0d6edd93
Dynesshely <catrol@qq.com>
2023-05-18 17:34:03 +0800

    💾 📦 Feat, Struct(TestPlugin.CSharp): Basic test plugin for CSharp.

---
+++ TestPlugin.CSharp/Controller.cs
@@ -0,0 +1,50 @@
+﻿using KitX.Contract.CSharp;
+using KitX.Web.Rules;
+using System.Text.Json;
+
+namespace TestPlugin.CSharp;
+
+public class Controller : IController
+{
+    private Queue<Command>? commands;
+
+    public void Start()
+    {
+        Console.WriteLine("Started");
+    }
+
+    public void Pause()
+    {
+        Console.WriteLine("Paused");
+    }
+
+    public void End()
+    {
+        Console.WriteLine("Ended");
+    }
+
+    public void Execute(Command cmd)
+    {
+        Console.WriteLine($"Execute: {JsonSerializer.Serialize(cmd)}");
+    }
+
+    public List<Function> GetFunctions()
+    {
+        return new();
+    }
+
+    public void SetCommandsSendBuffer(ref Queue<Command> commands)
+    {
+        this.commands = commands;
+    }
+
+    public void SetRootPath(string path)
+    {
+        Console.WriteLine($"Root path: {path}");
+    }
+
+    public void SetWorkPath(string path)
+    {
+        Console.WriteLine($"Work path: {path}");
+    }
+}
---
+++ TestPlugin.CSharp/IdentityInterface.cs
@@ -0,0 +1,81 @@
+﻿using KitX.Contract.CSharp;
+
+namespace TestPlugin.CSharp;
+
+public class IdentityInterface : IIdentityInterface
+{
+    public string GetName() => "TestPlugin.CSharp";
+
+    public string GetVersion() => "v0.1.0";
+
+    public Dictionary<string, string> GetDisplayName() => new()
+        {
+            { "zh-cn", "适用于 C# 的 KitX 测试用插件" },
+            { "zh-cnt", "適用於 C# 的 KitX 測試用插件" },
+            { "en-us", "KitX Testing Plugin for C#" },
+        };
+
+    public string GetAuthorName() => "Dynesshely";
+
+    public string GetPublisherName() => "Crequency";
+
+    public string GetAuthorLink() => "https://blog.catrol.cn";
+
+    public string GetPublisherLink() => "https://catrol.cn";
+
+    public Dictionary<string, string> GetSimpleDescription() => new()
+        {
+            { "zh-cn", "KitX 测试用插件" },
+            { "zh-cnt", "KitX 測試用插件" },
+            { "en-us", "KitX Test Plugin" },
+        };
+
+    public Dictionary<string, string> GetComplexDescription() => new()
+        {
+            { "zh-cn", "KitX 测试用插件, 使用 KitX.Loader.CSharp 作为加载器." },
+            { "zh-cnt", "KitX 測試用插件, 使用 KitX.Loader.CSharp 作爲加載器." },
+            { "en-us", "KitX Test Plugin, use KitX.Loader.CSharp as loader." },
+        };
+
+    public Dictionary<string, string> GetTotalDescriptionInMarkdown() => new()
+        {
+            {
+                "zh-cn",
+                """
+                # 关于
+                这是一个适用于 C# 的 KitX 测试用插件
+                使用 KitX.Loader.CSharp 作为加载器
+                """
+            },
+            {
+                "zh-cnt",
+                """
+                # 關於
+                這是一個適用於 C# 的 KitX 測試用插件
+                使用 KitX.Loader.CSharp 作爲加載器
+                """
+            },
+            {
+                "en-us",
+                """
+                # About
+                This is a KitX Test Plugin for C#
+                Use KitX.Loader.CSharp as loader
+                """
+            },
+        };
+
+    public string GetIconInBase64() => "图标";
+
+    public DateTime GetPublishDate() => DateTime.Now;
+
+    public DateTime GetLastUpdateDate() => DateTime.Now;
+
+    public IController GetController() => new Controller();
+
+    public bool IsMarketVersion() => false;
+
+    public IMarketPluginContract GetMarketPluginContract() => null;
+
+    public string GetRootStartupFileName() => "TestPlugin.CSharp.dll";
+}
